5d60fc3736aaae7a1f8aaee4ceb2fe6bafbe60d7
Otavio Beividas <88750691+OtavioBeividas@users.noreply.github.com>
2023-01-23 10:54:53 -0300

    Code in english

--- Cart.py
+++ Cart.py
@@ -1,105 +1,105 @@
-# listas
-itensMercearia = ['Arroz 5kg', 'Feijao 1kg', 'Cafe 500g', 'Macarrao 2kg']
-precoMercearia = [20.0, 8.0, 15.0, 9.0]
-itensLaticinios = ['Leite', 'Yogurte', 'Cheddar']
-precoLaticinios = [4.0, 2.5, 4.0]
-itensCongelados = ['Patinho', 'Coxa de Frango', 'Fraldinha']
-precoCongelados = [40.0, 15.0, 13.0]
-produtosCarrinho = []
-valorCarrinho = []
+# lists
+groceryItems = ['Arroz 5kg', 'Feijao 1kg', 'Cafe 500g', 'Macarrao 2kg']
+groceryPrice = [20.0, 8.0, 15.0, 9.0]
+dairyItems = ['Leite', 'Yogurte', 'Cheddar']
+dairyPrice = [4.0, 2.5, 4.0]
+frozenFoods = ['Patinho', 'Coxa de Frango', 'Fraldinha']
+frozenFoodPrice = [40.0, 15.0, 13.0]
+cartProducts = []
+cartValue = []
 total = 0
 
 
-# menu produtos
-def produtos():
-    print("\n\tSelecione uma das categorias abaixo :"
-          "\n\n\t================ Produtos ==============="
-          "\n\t|\t1 - Mercearia \t\t\t|"
-          "\n\t|\t2 - Laticinios \t\t\t|"
-          "\n\t|\t3 - Congelados \t\t\t|"
+# menu produtos // products menu
+def products():
+    print("\n\tSelect one of the categories below:"
+          "\n\n\t================ Products ==============="
+          "\n\t|\t1 - Grocery Store \t\t\t|"
+          "\n\t|\t2 - Dairy Products \t\t\t|"
+          "\n\t|\t3 - Frozen Foods \t\t\t|"
           "\n\t========================================="
           )
 
 
-# menu mercearia
-def mercearia():
+# menu mercearia // grocery menu
+def groceryStore():
     i = 0
-    tamMercearia = len(itensMercearia)
-    while i < tamMercearia:
-        print(f'Produto {i + 1}: {itensMercearia[i]} - Preço: R${precoMercearia[i]}')
+    sizeGrocery = len(groceryItems)
+    while i < sizeGrocery:
+        print(f'Product {i + 1}: {groceryItems[i]} - Price: R${groceryPrice[i]}')
         i += 1
-    produto = int(input('\nEscolha um produto: '))
-    atualizarLista(itensMercearia[produto - 1], precoMercearia[produto - 1])
+    product = int(input('\nChoose a product: '))
+    updateList(groceryItems[product - 1], groceryPrice[product - 1])
 
 
-# menu laticinios
-def laticinios():
+# menu laticinios // dairy menu
+def dairyProducts():
     i = 0
-    tamLaticinios = len(itensLaticinios)
-    while i < tamLaticinios:
-        print(f'Produto {i + 1}: {itensLaticinios[i]} - Preço: R${precoLaticinios[i]}')
+    dairySize = len(dairyItems)
+    while i < dairySize:
+        print(f'Product {i + 1}: {dairyItems[i]} - Price: R${dairyPrice[i]}')
         i += 1
-    produto = int(input('\nEscolha um produto: '))
-    atualizarLista(itensLaticinios[produto - 1], precoLaticinios[produto - 1])
+    product = int(input('\nChoose a product: '))
+    updateList(dairyItems[product - 1], dairyPrice[product - 1])
 
 
 
-# menu congelados
-def congelados():
+# menu congelados // frozen menu
+def frozenFoods():
     i = 0
-    tamCongelados = len(itensCongelados)
-    while i < tamCongelados:
-        print(f'Produto {i + 1}: {itensCongelados[i]} - Preço: R${precoCongelados[i]}')
+    frozenSize = len(frozenFoods)
+    while i < frozenSize:
+        print(f'Produto {i + 1}: {frozenFoods[i]} - Preço: R${frozenFoodPrice[i]}')
         i += 1
-    produto = int(input('\nEscolha um produto: '))
-    atualizarLista(itensCongelados[produto - 1], precoCongelados[produto - 1])
+    product = int(input('\nChoose a product: '))
+    updateList(frozenFoods[product - 1], frozenFoodPrice[product - 1])
 
 
-# adicionar produto ao carrinho
-def atualizarLista(nome, preco):
+# adicionar produto ao carrinho // add product to cart
+def updateList(name, price):
     global total
-    produtosCarrinho.append(nome)
-    valorCarrinho.append(preco)
-    total += preco
-    print('\nProduto adicionado ao carrinho com sucesso!')
-    print(f'\nProduto: {nome} - Preço: {preco}')
+    cartProducts.append(name)
+    cartValue.append(price)
+    total += price
+    print('\nProduct added to cart successfully!')
+    print(f'\nProduct: {name} - Price: {price}')
 
     
-# checar carrinho
-def verCarrinho():
+# checar carrinho // check cart
+def viewCart():
     i = 0
-    while i < len(produtosCarrinho):
-        print(f'Produto {i + 1}: {produtosCarrinho[i]} - Preço: R${valorCarrinho[i]}')
+    while i < len(cartProducts):
+        print(f'Product {i + 1}: {cartProducts[i]} - Price: R${cartValue[i]}')
         i += 1
     print(f'\nValor total: R${total}')
 
 
 while True:
     opcao = int(input(
-        "\n\n\t========= Bem-vindo ao SuperMarket ======"
-        "\n\t\t1 - Produtos \t\t\t"
-        "\n\t\t2 - Ver carrinho \t\t"
-        "\n\t\t0 - Sair\t\t\t"
+        "\n\n\t========= Welcome to the SuperMarket ======"
+        "\n\t\t1 - Products \t\t\t"
+        "\n\t\t2 - View Cart \t\t"
+        "\n\t\t0 - Exit\t\t\t"
         "\n\t========================================="
-        "\n\nEscolha uma opcao: "
+        "\n\nChoose an option: "
     ))
 
-    if opcao == 0:
-        print('Obrigado por nos visitar, volte sempre!')
+    if option == 0:
+        print('Thanks for visiting us, come back often!')
         break
     else:
-        match opcao:
+        match option:
             case 1:
-                produtos()
-                escolha = int(input('Escolha uma opção: '))
-                match escolha:
+                products()
+                choice = int(input('Choose an option: '))
+                match choice:
                     case 1:
-                        mercearia()
+                        groceryStore()
                     case 2:
-                        laticinios()
+                        dairyProducts()
                     case 3:
-                        congelados()
+                        frozenFoods()
                     case _:
-                        print('Digite uma escolha válida')
+                        print('Enter a valid choice')
             case 2:
-                verCarrinho()
+                viewCart()
